2bea3a45e4c49cdb1c8e0ce6c38a57d2fd5467cf
davidpopovich <davidpopovich.yesodot@gmail.com>
2026-05-24 14:27:21 +0300

    finish project

---
+++ file_generator.ts
@@ -0,0 +1,28 @@
+import fs from "fs/promises";
+import path from "path";
+import { generate } from "random-words";
+
+const file_generator = async (numFile: number, numTxt: number) => {
+  try {
+    let textWritingNum = numTxt;
+    const dirName = "created_files";
+    await fs.mkdir(dirName, { recursive: true });
+    for (let i = 0; i < numFile; i++) {
+      const fileName = `file${i + 1}.txt`;
+      const filePath = path.join(dirName, fileName);
+      await fs.writeFile(
+        filePath,
+        generate({ exactly: textWritingNum, join: " " }),
+        "utf-8",
+      );
+      console.log(`file name: ${fileName}, sum words writ: ${textWritingNum}.`);
+      textWritingNum *= 2;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const numberFile = parseInt(process.argv[2], 10);
+const numberTxt = parseInt(process.argv[3], 10);
+file_generator(numberFile, numberTxt);
---
+++ file_mover.ts
@@ -0,0 +1,46 @@
+import { watch } from "fs";
+import fs from "fs/promises";
+import path from "path";
+
+const pathFilesToMove = path.join(import.meta.dirname, "files_to_move");
+const pathMovedFiles = path.join(import.meta.dirname, "moved_files");
+const pathMovedFileTxt = path.join(import.meta.dirname, "moved_files.txt");
+
+const moveFile = async (fileName: string) => {
+  try {
+    const oldPath = path.join(pathFilesToMove, fileName);
+    const newPath = path.join(pathMovedFiles, fileName);
+    await fs.access(oldPath);
+    await fs.rename(oldPath, newPath);
+    console.log(`file: -${fileName}- moving to -moved_files-.`);
+    await fs.appendFile(pathMovedFileTxt, fileName + "\n", "utf8");
+  } catch (error: any) {
+    if (error.code !== "ENOENT") {
+      console.error(`Error moving ${fileName}:`, error);
+    }
+  }
+};
+
+const movingFiles = async () => {
+  try {
+    await fs.mkdir(pathFilesToMove, { recursive: true });
+    await fs.mkdir(pathMovedFiles, { recursive: true });
+    await fs.writeFile(pathMovedFileTxt, "", { flag: "a" });
+    console.log(`The system is running and listening to -files_to_move- :`);
+
+    const filesToMove = await fs.readdir(pathFilesToMove);
+    for (const fileName of filesToMove) {
+      await moveFile(fileName);
+    }
+    watch(pathFilesToMove, async (_eventType, fileName) => {
+      if (fileName) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        await moveFile(fileName);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+movingFiles();
